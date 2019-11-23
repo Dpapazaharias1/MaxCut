6afedc9c0decdaf5e3d02d373bd689fb4a882eb0
Demetrios Papazaharias <dpapazaharias1@gmail.com>
2019-11-23 10:34:39 -0500

    First commit

---
+++ src/Graph.py
@@ -0,0 +1,35 @@
+class Graph:
+    n = 0
+    m = 0
+    adjLists = []
+    adjWeight = []
+    adjMatrix = []
+    totWeight = 0
+
+    def read(self, inputfile):
+        f = open(inputfile, 'r')
+        line = f.readline()
+        fields = str.split(line)
+        self.n = int(fields[0])
+        self.m = int(fields[1])
+
+        for i in range(self.n):
+            self.adjLists.append([])
+            self.adjWeight.append([])
+            self.adjMatrix.append([0 for i in range(self.n)])
+
+        for line in f:
+            fields = line.split('\t')
+            i = int(fields[0])
+            j = int(fields[1])
+            self.adjLists[i].append(j)
+            self.adjLists[j].append(i)
+            w_e = 1
+            if len(fields) > 2:
+                w_e = float(fields[2])
+            self.totWeight += abs(w_e)
+            self.adjWeight[i].append(w_e)
+            self.adjWeight[j].append(w_e)
+            self.adjMatrix[i][j] = w_e
+            self.adjMatrix[j][i] = w_e
+        f.close
---
+++ src/MaxCut_IP.py
@@ -0,0 +1,53 @@
+from gurobipy import *
+from Graph import *
+import os
+
+
+class MaxCut:
+    G = Graph()
+    n = 0
+    m = 0
+    model = 0
+    y = {}
+    x = {}
+    
+    def __initializeModel(self):
+        self.model = Model("MaxCut")
+        #---- Variables
+        # TODO: Can we relax the binary variables for y's.
+        for i in range(self.n):
+            self.x[i] = self.model.addVar(vtype=GRB.BINARY, obj = 0, name='vertex_{}'.format(i))
+            for j in self.G.adjLists[i]:
+                if i < j:
+                    self.y[i, j] = self.model.addVar(vtype=GRB.BINARY, obj = self.G.adjMatrix[i][j], name='edge_{}_{}'.format(i,j))
+        #---- Model Parameters
+        self.model.modelSense = GRB.MAXIMIZE
+        self.model.update()
+        #---- Constraints
+        # TODO: Check if constraints (4) and (5) are redundant.
+        for i in range(self.n):
+            for j in self.G.adjLists[i]:
+                if i < j:
+                    self.model.addConstr(self.y[i,j] <= 2 - self.x[i] - self.x[j])
+                    self.model.addConstr(self.y[i,j] <= self.x[i] + self.x[j])
+                    self.model.addConstr(self.y[i,j] >= self.x[i] - self.x[j]) # Possibly redundant
+                    self.model.addConstr(self.y[i,j] >= self.x[j] - self.x[i]) # Possibly redundant
+
+
+    def initialize(self, inputfile):
+        self.G.read(inputfile)
+        self.n = self.G.n
+        self.m = self.G.m
+        self.__initializeModel()
+
+    def solve(self):
+        self.model.optimize()
+
+if __name__ == "__main__":
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+    instance = "../dat/Graph_instance_n_10_d_0.2_s_1.dat"
+    MC = MaxCut()
+    MC.initialize(instance)
+    MC.solve()
