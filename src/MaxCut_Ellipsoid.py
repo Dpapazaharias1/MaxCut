e4f1be99dd31f9dd58a135784d61fe2aacbf7f37
Demetrios Papazaharias <dpapazaharias1@gmail.com>
2019-12-16 15:37:43 -0500

    Ellipsoid

--- src/MaxCut_Ellipsoid.py
+++ src/MaxCut_Ellipsoid.py
@@ -1,149 +1,117 @@
 from Graph import Graph 
-import numpy as np 
-import numpy.linalg as lin 
-import math 
+import numpy as np
+import math
+import time
 
-### FUNCTIONS ###
 
-def checkFeasible(y):
-    # solve minimum v'Yv, using ybar matrix 
-    # import pdb; pdb.set_trace()
-    ybar = dictToMatrix(y)
-    ybar_eigvalues, ybar_eigvectors = lin.eig(ybar)
-    min_value = np.amin(ybar_eigvalues)
-    min_index = np.where(ybar_eigvalues == np.amin(ybar_eigvalues))
-    if min_value >= 0:
-        return None
-    else:
-        return ybar_eigvectors[min_index[0][0]]
+class Ellipsoid:
+    
+    G = Graph()
+    n = 0
+    m = 0
+    y = []
+    w = []
+    g = []
+    mapv = []
+    var = 0 # Number of variables Y_ij, not including diagonal and Y_ji
+    H = 0
+    R = 0
+    Q = [] # List of eigenvectors for PSD constraints
+    change = 100
+    epsilon = 0.01
+    objCurrent = 0
+    Runtime = 0
 
-def computeConstrGradient(y, q, var):
-    # compute gbar for next step
-    gbar = np.zeros(var)
-    counter = 0 
-    for key in y:
-        ij_string = key.split(',')
-        i = int(ij_string[0])
-        j = int(ij_string[1])
-        gbar[counter] = q[i] + q[j]
-        counter += 1
-    return gbar
+    def initialize(self, inputfile):
+        self.G.read(inputfile)
+        self.n = self.G.n
+        self.m = self.G.m
+        counter = 0
+        for i in range(self.n):
+            for j in range(i+1, self.n):
+                    self.y.append(0)
+                    self.w.append(self.G.adjMatrix[i][j])
+                    self.g.append(0.5*self.G.adjMatrix[i][j])
+                    self.mapv.append((i,j))
+                    counter += 1
+        
+        self.y = np.array([self.y]).T
+        self.g = np.array([self.g]).T
+        self.var = len(self.y)
+        self.R = 2*self.n
+        self.H = (self.R**2) * np.eye(self.var)
 
-def computeY(y, H, g):
-    # compute next Y
-    Hg = H.dot(g) # numerator of big fraction
-    Hdenom = math.sqrt(g.T.dot(Hg)) # denominator
-    subtract = (1/G.n+1)*(1/Hdenom)*Hg # entire subtracion term
-    yvec = dictToVector(y, mapv) # flip to vector
-    yvec = yvec - subtract # subtract
-    return vectorToDict(yvec, mapv) # return new variables (dictionary)
+    def __computeObj(self):
+        zcurr = 0
+        for i in range(self.var):
+            zcurr += self.w[i] * (1-self.y[i])
+        zcurr = 0.5*zcurr
+        return zcurr[0]
 
-def computeH(y, H, g):
-    # compute next H
-    HggtH = ((H.dot(g)).dot(g.T))*H # numerator of big fraction
-    Hdenom = g.T.dot(H.dot(g)) # denominator of big fraction
-    subtract = (2/(G.n+1))*(1/Hdenom)*HggtH
-    H = H - subtract
-    return ((G.n**2)/((G.n**2)-1))*H
+    def __listToMatrix(self):
+        yBar = np.eye(self.n)
+        counter = 0
+        for i in range(self.n):
+            for j in range(i+1, self.n):
+                yBar[i][j] = self.y[counter]
+                yBar[j][i] = self.y[counter]
+                counter += 1
+        return yBar
 
-def computeObj(w, y):
-    # compute current objective value
-    zcurr = 0
-    for key in y:
-        zcurr += w[key]*(1-y[key])
-    zcurr = -0.5*zcurr
-    return zcurr
-
-def dictToVector(y, mapv):
-    # convert dictionary y to vector y for matrix ops
-    yvec = np.zeros(var)
-    for key in y:
-        yvec[mapv[key]] = y[key]
-    return yvec
-
-def vectorToDict(yvec, mapv):
-    # convert y vector to a dictionary of decision variables
-    y = {}
-    for key in mapv:
-        y[key] = yvec[mapv[key]]
-    return y 
-
-def dictToMatrix(y):
-    # take dict y and turn it into ybar matrix
-    # ones on the diagonal (variable ii, not used), yij in spot yij
-    # matrix needed for eigenvector operations to check feasibility
-    ybar = np.eye(G.n)
-    for key in y:
-        ij_string = key.split(',')
-        i = int(ij_string[0])
-        j = int(ij_string[1])
-        ybar[i][j] = y[key]
-    return ybar
-
-# read instance
-G = Graph()
-G.read('Graphtest.txt')
-
-# initialize variables and parameters
-y = {} # decision variable y dictionary
-w = {} # weights dictionary
-mapv = {} # dictionary to map variable key names "i,j" to index in vectors
-counter = 0
-for i in range(G.n):
-    for j in range(G.n):
-        if i < j:
-            y["{},{}".format(i, j)] = 0
-            w["{},{}".format(i, j)] = G.adjMatrix[i][j] 
-            mapv["{},{}".format(i, j)] = counter
-            counter += 1
+    def __checkFeasible(self):
+        yBar = self.__listToMatrix()
+        eigValues, eigVectors = np.linalg.eig(yBar)
+        minEig = np.amin(eigValues)
+        minIndex = np.where(eigValues == minEig)[0][0]
+        if minEig < 0:
+            return eigVectors[:, minIndex]
         else:
-            pass
-var = len(y) # number of variables in y, without double counting or counting "ii"s
-g = np.zeros(var) # initialize constant objective function gradient
-counter = 0
-for key in w:
-    g[counter] = -w[key]
-    counter += 1
-H = np.eye(var) # starting ellipsoid, circle of radius r
-Q = [] # keep track of eigenvectors used
-r = math.sqrt(G.m)
-H = r*H
-epsilon = 0.00000001
-change = 100
-zcurr = computeObj(w, y)
+            return np.array([])
+    
+    def __getConstrGradient(self, q):
+        gBar = np.zeros((self.var,1))
+        counter = 0
+        for i in range(self.n):
+            for j in range(i+1, self.n):
+                gBar[counter] = -q[i] * q[j]
+                counter += 1
+        return gBar
 
-#####################################################################################
-##################################### MAIN LOOP #####################################
-#####################################################################################
+    def __update(self, y, g):
+        Hg = self.H.dot(g)
+        gTH = g.T.dot(self.H)
+        HggTH = Hg.dot(gTH)
+        Hdenom = g.T.dot(Hg)
+        ySubtract = (1.0/(self.var + 1)) * (1.0/math.sqrt(Hdenom)) * Hg
+        self.y = y - ySubtract
+        hSubtract = (2.0/(self.var+1)) * (1.0/float(Hdenom)) * HggTH
+        newH = self.H - hSubtract
+        self.H = ((self.var**2)/((self.var**2) - 1))*newH
 
-counter = 0
-print("Initial y: {}".format(y))
-while change > epsilon:
-    qvector = checkFeasible(y)
-    if qvector == None: 
-        y = computeY(y, H, g)
-        H = computeH(y, H, g)
-    else: 
-        gbar = computeConstrGradient(y, qvector, var)
-        Q.append(qvector)
-        y = computeY(y, H, gbar)
-        H = computeH(y, H, gbar)
-    # print("Iteration {}: {}".format(counter, y))
-    z_old = zcurr
-    zcurr = computeObj(w, y)
-    change = abs(z_old - zcurr)
-    counter += 1
+    def solve(self):
+        start = time.time()
+        counter = 0
+        while self.change > self.epsilon:
+            yCurrent = np.copy(self.y)
+            qNew = self.__checkFeasible()
+            if qNew.size > 0:
+                self.Q.append(qNew)
+                gBar = self.__getConstrGradient(qNew)
+                self.__update(yCurrent, gBar)
+            else:
+                self.__update(yCurrent, self.g)
+                self.objCurrent = self.__computeObj()
+            self.change = np.linalg.norm(yCurrent-self.y)
+            counter += 1
+        self.Runtime = time.time() - start
 
-print("Iterations: {}".format(counter))
-print("Solution: {}".format(y))
-print("Objective: {}".format(zcurr))
-print("Eigenvectors added: {}".format(Q))
-print(w)
-
-#####################################################################################
-#####################################################################################
-#####################################################################################
-
-
-
-
+if __name__ == "__main__":
+    import os
+    import time
+    SDP = Ellipsoid()
+    SDP.initialize('../dat/Graph_instance_n_10_d_0.2_s_2.dat')
+    start = time.time()
+    SDP.solve()
+    print('Runtime: {}'.format(SDP.Runtime))
+    #print(SDP.objBest)
+    print(SDP.objCurrent)
